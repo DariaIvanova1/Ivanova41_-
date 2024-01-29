e4e335630bc805909331d91e31638f3eb54050d3
Дарья <@DESKTOP-J8VN7UA>
2024-01-29 18:45:32 +0500

    Добавьте файлы проекта.

---
+++ Ivanova41_Размер/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace Ivanova41_Размер
+{
+    internal class Manager
+    {
+       public static Frame MainFrame { get;  set; }
+    }
+}
